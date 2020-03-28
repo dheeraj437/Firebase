c62d54b8049bbd5ad25a57df14d05309de1602ad
dheeraj437 <37209186+dheeraj437@users.noreply.github.com>
2020-03-28 15:18:02 +0530

    Add files via upload

---
+++ getcustomer.php
@@ -0,0 +1,26 @@
+<?php
+$mysqli = new mysqli("remotemysql.com", "x7jsztqjaf", "i3M0j1fKai", "x7jsztqjaf");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SELECT customerid, companyname
+FROM customers WHERE customerid = ?";
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($cid, $cname );
+$stmt->fetch();
+$stmt->close();
+
+echo "<table>";
+echo "<tr>";
+echo "<th>CustomerID</th>";
+echo "<td>" . $cid . "</td>";
+echo "<th>CompanyName</th>";
+echo "<td>" . $cname . "</td>";
+echo "</tr>";
+echo "</table>";
+?>
