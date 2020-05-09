57d45daadf2759543908c7f5ad6d5eeb12f479b7
dheeraj437 <37209186+dheeraj437@users.noreply.github.com>
2020-05-09 20:29:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,13 +58,18 @@ $(document).ready(function () {
         <td>${document.data().father}</td>
         <td>${document.data().khata}</td>
         <td>${document.data().survey}</td>
-        <td>${document.data().crop}</td>
-        <td>${document.data().phone}</td>
-        <td>${document.data().aadhaar}</td>
-        <td>${document.data().account}</td>
-        <td>${document.data().ifsc}</td>
-        <td>${document.data().bank}</td>
+        <td "id=one" >${document.data().crop}</td>
+        <td "id=one">${document.data().phone}</td>
+        <td "id=one">${document.data().aadhaar}</td>
+        <td "id=one">${document.data().account}</td>
+        <td "id=one">${document.data().ifsc}</td>
+        <td "id=one">${document.data().bank}</td>
 
+@media only screen and (max-width: 600px) {
+.one {
+display: block;
+}
+}
        
         <td>
             <a href="#" id="${document.id}" class="edit js-edit-employee"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
