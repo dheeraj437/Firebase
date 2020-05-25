70d44e337ddb8cb1e9c657ffde915084c705b983
dheeraj437 <37209186+dheeraj437@users.noreply.github.com>
2020-05-25 19:48:07 +0530

    Update view.js

--- js/view.js
+++ js/view.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
         <td>${document.data().name}</td>
         <td>${document.data().father}</td>
 
-        <td><button style="background-color: green;" ><input type="button"  style=" width:100%; height:100%; background-color: green; color: white; border: none;" value="${document.data().bank}"></button></td>
+        <td><button style="background-color: green; border: none; " ><input type="button"  style=" width:100%; height:100%; background-color: green; color: white; border: none;" value="${document.data().bank}"></button></td>
 
        
         
